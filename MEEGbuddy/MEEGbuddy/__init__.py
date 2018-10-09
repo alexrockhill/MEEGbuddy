8eefa6e5c51af2e69564643969a2766442160cf2
Alexander P Rockhill <apr24@karima.nmr.mgh.harvard.edu>
2018-10-09 15:47:50 -0400

    source correlation

---
+++ MEEGbuddy/MEEGbuddy/__init__.py
@@ -0,0 +1,6 @@
+""" MEEG Resources for MGH Division of Neurotherapeutics """
+
+from .meeg import MEEGbuddy,Comparator,create_demi_events
+from . import pci
+from .psd_multitaper_plot_tools import DraggableResizeableRectangle,ButtonClickProcessor
+from .gif_combine import combine_gifs
